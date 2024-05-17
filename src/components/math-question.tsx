6a0338556b733f780d93f8d1384cd522874245a8
Max <hellhunter15@hotmail.com>
2024-05-17 22:00:38 +0200

    fix: answer showing next to math

---
+++ src/components/math-question.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useState } from "react";
+import { MathQuestionProps } from "./math-questions";
+
+export function MathQuestion(props: MathQuestionProps) {
+	const [inputAnswer, setInputAnswer] = useState<number | null>(null);
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputAnswer(Number(e.target.value));
+	};
+
+	return (
+		<div className="flex flex-row justify-start p-4">
+			<p className="pr-2">
+				{props.firstNumber} {props.operator} {props.secondNumber} =
+			</p>
+			<input
+				type="number"
+				value={inputAnswer ?? ""}
+				onChange={handleChange}
+			/>
+			{inputAnswer !== null && (
+				<p
+					className={`font-bold pl-2 ${
+						props.answer === inputAnswer
+							? "text-green-600"
+							: "text-red-600"
+					}`}
+				>
+					{props.answer === inputAnswer ? "GOED!" : "FOUT!"}
+				</p>
+			)}
+		</div>
+	);
+}
--- src/components/math-questions.tsx
+++ src/components/math-questions.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useState } from "react";
+import { MathQuestion } from "./math-question";
 
-type MathQuestionProps = {
+export type MathQuestionProps = {
 	firstNumber: number;
 	secondNumber: number;
 	operator: string;
@@ -38,39 +39,6 @@ export default function MathQuestions(props: MathType) {
 			{questions.map((question, index) => (
 				<MathQuestion key={index} {...question} />
 			))}
-		</div>
-	);
-}
-
-function MathQuestion(props: MathQuestionProps) {
-	const [inputAnswer, setInputAnswer] = useState<number | null>(null);
-
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setInputAnswer(Number(e.target.value));
-	};
-
-	return (
-		<div className="flex flex-row justify-start p-4">
-			<p className="pr-2">
-				{props.firstNumber} {props.operator} {props.secondNumber} ={" "}
-				{props.answer}
-			</p>
-			<input
-				type="number"
-				value={inputAnswer ?? ""}
-				onChange={handleChange}
-			/>
-			{inputAnswer !== null && (
-				<p
-					className={`font-bold pl-2 ${
-						props.answer === inputAnswer
-							? "text-green-600"
-							: "text-red-600"
-					}`}
-				>
-					{props.answer === inputAnswer ? "GOED!" : "FOUT!"}
-				</p>
-			)}
 		</div>
 	);
 }

f4b01176b5d461c1fd15890d409ea0e55e107ec0
Max <39444801+HellHunterMax@users.noreply.github.com>
2025-07-18 14:21:39 +0200

    fix: table equations not correctly using max table (#28)
    Co-authored-by: Max Buining <mbuining@rgfstaffing.nl>

--- src/hooks/useMath.tsx
+++ src/hooks/useMath.tsx
@@ -2,94 +2,94 @@ import mathQuestionProps from "@/components/shared/models/math-question-props";
 import { Operator } from "@/enums/operator";
 
 const useMath = (operator: Operator, maxAnswer: number) => {
-	const generateMathEquation = (id: number): mathQuestionProps => {
-		switch (operator) {
-			case Operator.Add:
-				return generateAddEquation(id);
-			case Operator.Subtract:
-				return generateSubtractEquation(id);
-			case Operator.Multiply:
-				return generateMultiplyEquation(id);
-			case Operator.Divide:
-				return generateDivideEquation(id);
-			default:
-				throw new Error("Invalid operator");
-		}
-	};
+  const generateMathEquation = (id: number): mathQuestionProps => {
+    switch (operator) {
+      case Operator.Add:
+        return generateAddEquation(id);
+      case Operator.Subtract:
+        return generateSubtractEquation(id);
+      case Operator.Multiply:
+        return generateMultiplyEquation(id);
+      case Operator.Divide:
+        return generateDivideEquation(id);
+      default:
+        throw new Error("Invalid operator");
+    }
+  };
 
-	const generateAddEquation = (id: number): mathQuestionProps => {
-		const answer = Math.floor(Math.random() * (maxAnswer + 1));
-		const firstNumber = Math.floor(Math.random() * (answer + 1));
-		const secondNumber = answer - firstNumber;
+  const generateAddEquation = (id: number): mathQuestionProps => {
+    const answer = Math.floor(Math.random() * (maxAnswer + 1));
+    const firstNumber = Math.floor(Math.random() * (answer + 1));
+    const secondNumber = answer - firstNumber;
 
-		return {
-			id,
-			firstNumber,
-			secondNumber,
-			operator,
-			answer,
-		};
-	};
+    return {
+      id,
+      firstNumber,
+      secondNumber,
+      operator,
+      answer,
+    };
+  };
 
-	const generateSubtractEquation = (id: number): mathQuestionProps => {
-		const firstNumber = Math.floor(Math.random() * (maxAnswer + 1));
-		const secondNumber = Math.floor(Math.random() * (firstNumber + 1));
-		const answer = calculate(firstNumber, secondNumber, operator);
+  const generateSubtractEquation = (id: number): mathQuestionProps => {
+    const firstNumber = Math.floor(Math.random() * (maxAnswer + 1));
+    const secondNumber = Math.floor(Math.random() * (firstNumber + 1));
+    const answer = calculate(firstNumber, secondNumber, operator);
 
-		return {
-			id,
-			firstNumber,
-			secondNumber,
-			operator,
-			answer,
-		};
-	};
+    return {
+      id,
+      firstNumber,
+      secondNumber,
+      operator,
+      answer,
+    };
+  };
 
-	const generateMultiplyEquation = (id: number): mathQuestionProps => {
-		const firstNumber = Math.floor(Math.random() * maxAnswer) + 1;
-		const secondNumber = Math.floor(Math.random() * 10) + 1;
-		const answer = firstNumber * secondNumber;
+  const generateMultiplyEquation = (id: number): mathQuestionProps => {
+    const firstNumber = Math.floor(Math.random() * 10) + 1;
+    const secondNumber = Math.floor(Math.random() * maxAnswer) + 1;
+    const answer = firstNumber * secondNumber;
 
-		return {
-			id,
-			firstNumber,
-			secondNumber,
-			operator,
-			answer,
-		};
-	};
+    return {
+      id,
+      firstNumber,
+      secondNumber,
+      operator,
+      answer,
+    };
+  };
 
-	const generateDivideEquation = (id: number): mathQuestionProps => {
-		const secondNumber = Math.floor(Math.random() * 10) + 1; // ensuring secondNumber is at least 1
-		const answer = Math.floor(Math.random() * maxAnswer) + 1; // ensuring answer is at least 1
-		const firstNumber = secondNumber * answer; // ensuring firstNumber is a multiple of secondNumber
+  const generateDivideEquation = (id: number): mathQuestionProps => {
+    const answer = Math.floor(Math.random() * 10) + 1;
+    const secondNumber = Math.floor(Math.random() * maxAnswer) + 1;
+    const firstNumber = secondNumber * answer;
 
-		return {
-			id,
-			firstNumber,
-			secondNumber,
-			operator,
-			answer,
-		};
-	};
+    return {
+      id,
+      firstNumber,
+      secondNumber,
+      operator,
+      answer,
+    };
+  };
 
-	const calculate = (a: number, b: number, operator: Operator): number => {
-		switch (operator) {
-			case Operator.Add:
-				return a + b;
-			case Operator.Subtract:
-				return a - b;
-			case Operator.Multiply:
-				return a * b;
-			default:
-				throw new Error("Invalid Operator");
-		}
-	};
+  const calculate = (a: number, b: number, operator: Operator): number => {
+    switch (operator) {
+      case Operator.Add:
+        return a + b;
+      case Operator.Subtract:
+        return a - b;
+      case Operator.Multiply:
+        return a * b;
+      default:
+        throw new Error("Invalid Operator");
+    }
+  };
 
-	return {
-		calculate,
-		generateMathEquation,
-	};
+  return {
+    calculate,
+    generateMathEquation,
+  };
 };
 
 export default useMath;

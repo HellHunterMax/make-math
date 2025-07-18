7f108374066b2a27e5a8a5af60a9d821e3397b91
Max Buining <mbuining@rgfstaffing.nl>
2025-07-18 15:07:39 +0200

    chore: upgrade NextJs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,25 +6,21 @@ import NavigationMenu from "@/components/Navigation/Menu/navigation-menu";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Make Math",
-	description: "Created by Max",
+  title: "Make Math",
+  description: "Created by Max",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" className="h-full w-full">
-			<body
-				className={`${inter.className} h-full w-full bg-slate-300 text-black`}
-			>
-				<NavigationMenu />
-				<div className="flex min-h-full min-w-full flex-col items-center absolute p-4 md:pl-60 pt-14">
-					{children}
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} h-full w-full bg-slate-300 text-black`} style={{ minHeight: "100vh", width: "100%" }}>
+        <NavigationMenu />
+        <div className="flex min-h-full min-w-full flex-col items-center absolute p-4 md:pl-60 pt-14">{children}</div>
+      </body>
+    </html>
+  );
 }
